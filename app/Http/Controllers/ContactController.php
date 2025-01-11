ce7296b0a2fc767e1d2d8e08b47152e000595154
Rajesh Mondal <rajesh01749@gmail.com>
2025-01-11 23:54:49 +0600

    controller created and write all necessary methods

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpParser\Builder\Function_;
+
+class ContactController extends Controller
+{
+    function page( Request $request ) {
+
+    }
+
+    function contactRequest( Request $request ) {
+
+    }
+}
