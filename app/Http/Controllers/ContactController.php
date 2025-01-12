95a8b01d46723fa6771a28212396f285602602d7
Rajesh Mondal <rajesh01749@gmail.com>
2025-01-13 00:35:11 +0600

    Add query for fetching project and contact data

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use PhpParser\Builder\Function_;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller
 {
     function page( Request $request ) {
-
+        return view( 'pages.contact' );
     }
 
     function contactRequest( Request $request ) {
-
+        return DB::table( 'contacts' )->insert( $request->input() );
     }
 }
