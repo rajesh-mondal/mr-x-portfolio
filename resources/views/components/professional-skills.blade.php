7a601f1fd9c185583db9ede7c04a1d34b6d62cfc
Rajesh Mondal <rajesh01749@gmail.com>
2025-02-07 16:47:46 +0600

    Dynamic rendering of skills list on resume page

--- resources/views/components/professional-skills.blade.php
+++ resources/views/components/professional-skills.blade.php
@@ -5,15 +5,8 @@
                 <h2 class="text-gradient fw-bolder mb-4">Professional Skills</h2>
                 <div class="card shadow border-0 rounded-4 mb-5">
                     <div class="card-body p-5">
-                        <div class="row row-cols-1 row-cols-md-3 mb-4">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">SEO/SEM Marketing</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Statistical Analysis</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Web Development</div></div>
-                        </div>
-                        <div class="row row-cols-1 row-cols-md-3">
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Network Security</div></div>
-                            <div class="col mb-4 mb-md-0"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">Adobe Software Suite</div></div>
-                            <div class="col"><div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">User Interface Design</div></div>
+                        <div id="skills-list" class="row row-cols-1 row-cols-md-3 mb-4">
+
                         </div>
                     </div>
                 </div>
@@ -21,3 +14,24 @@
         </div>
     </div>
 </div>
+
+<script>
+    SkillList();
+    async function SkillList() {
+        try {
+            let URL = "/skillsData"
+            let response = await axios.get(URL);
+            
+            response.data.forEach( (item) => {
+                document.getElementById('skills-list').innerHTML+=(`<div class="col mb-4 p-2 mb-md-0">
+                    <div class="d-flex align-items-center bg-light rounded-4 p-3 h-100">
+                        ${item['name']}
+                    </div>
+                </div>`)
+            });
+
+        } catch (error) {
+            alert(error);
+        }
+    }
+</script>
