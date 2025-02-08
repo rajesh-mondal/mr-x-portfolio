acd008363ea196e47c6b69315e31bf9d86de70d1
Rajesh Mondal <rajesh01749@gmail.com>
2025-02-08 21:51:33 +0600

    Dynamic rendering of experience list on resume page

--- resources/views/components/experience.blade.php
+++ resources/views/components/experience.blade.php
@@ -14,23 +14,42 @@
                     </a>
                 </div>
 
-                <div class="card shadow border-0 rounded-4 mb-5">
-                    <div class="card-body p-5">
-                        <div class="row align-items-center gx-5">
-                            <div class="col text-center text-lg-start mb-4 mb-lg-0">
-                                <div class="bg-light p-4 rounded-4">
-                                    <div class="text-primary fw-bolder mb-2">2019 - Present</div>
-                                    <div class="small fw-bolder">Web Developer</div>
-                                    <div class="small text-muted">Stark Industries</div>
-                                    <div class="small text-muted">Los Angeles, CA</div>
-                                </div>
-                            </div>
-                            <div class="col-lg-8"><div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus laudantium, voluptatem quis repellendus eaque sit animi illo ipsam amet officiis corporis sed aliquam non voluptate corrupti excepturi maxime porro fuga.</div></div>
-                        </div>
-                    </div>
+                <div id="experience-list">
+
                 </div>
 
             </section>
         </div>
     </div>
 </div>
+
+<script>
+    getExpList();
+    async function getExpList() {
+        try {
+            let URL = "/experiencesData"
+            let response = await axios.get(URL);
+
+            response.data.forEach(item => {
+                document.getElementById('experience-list').innerHTML+=(`<div class="card shadow border-0 rounded-4 mb-5">
+                    <div class="card-body p-5">
+                        <div class="row align-items-center gx-5">
+                            <div class="col text-center text-lg-start mb-4 mb-lg-0">
+                                <div class="bg-light p-4 rounded-4">
+                                    <div class="text-primary fw-bolder mb-2">${item['designation']}</div>
+                                    <div class="small fw-bolder mb-2">${item['title']}</div>
+                                    <div class="small text-muted">${item['duration']}</div>
+                                </div>
+                            </div>
+                            <div class="col-lg-8"><div>${item['details']}</div></div>
+                        </div>
+                    </div>
+                </div>`)
+
+            });
+
+        } catch (error) {
+            alert(error);
+        }
+    }
+</script>
