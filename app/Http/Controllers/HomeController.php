0b4b27ca31380c882f05287aa58e19e15eecab6b
Rajesh Mondal <rajesh01749@gmail.com>
2025-02-11 17:36:00 +0600

    Set SEO properties for improved search engine visibility

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
     function page( Request $request ) {
-        return view( 'pages.home' );
+        $seo = DB::table( 'seoproperties' )->where( 'pageName', '=', 'home' )->first();
+        return view( 'pages.home', ['seo' => $seo] );
     }
 
     function heroData( Request $request ) {
